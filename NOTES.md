3ccb18258d79ec0d82688492cdd695f6f7e077bd
Kosala Tennakoon <kosala.me@gmail.com>
2021-05-08 17:54:08 +0530

    added common regexes

---
+++ NOTES.md
@@ -0,0 +1,5 @@
+# Notes
+
+## RegEx Patterns
+- [Ultimate Regex Cheat Sheet](https://www.keycdn.com/support/regex-cheatsheet)
+- [Top 15 Commonly Used Regex](https://digitalfortress.tech/tricks/top-15-commonly-used-regex/)
--- patterns.go
+++ patterns.go
@@ -1,13 +1,18 @@
 package veil
 
-// TODO: add common patterns
-// Refer:
-// - https://www.keycdn.com/support/regex-cheatsheet
-// - https://digitalfortress.tech/tricks/top-15-commonly-used-regex/
 const (
 	// PatternNumber will match any number.
 	PatternNumber string = `[-]?[0-9]+[,.]?[0-9]*([\/][0-9]+[,.]?[0-9]*)*`
 
 	// PatternEmail will match any email address.
-	PatternEmail string = "email"
+	PatternEmail string = `^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$`
+
+	// PatternUUID4 will match any uuid4.
+	PatternUUID4 string = `[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}`
+
+	// PatternIPv4 will match any IPv4 address.
+	PatternIPv4 string = `^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`
+
+	// PatternIPv6 will match any IPv6 address.
+	PatternIPv6 string = `(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))`
 )
